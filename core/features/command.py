d313115b3d73386d165ab9f3926d79a2ed4fafe9
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2020-03-10 13:06:59 +0100

    update

--- core/features/command.py
+++ core/features/command.py
@@ -2,7 +2,14 @@
 
 def run_command(my_socket):
     print("[+] Running user Command")
-    command = input("[+] Enter command to run: ")
+    keep_running = True
+    print("[+] Enter Command ")
+    while keep_running:
+        command = input(">> ")
+        my_socket.send_data(command)
+        if command == "stop" or command == "exit":
+            keep_running = False
 
-
+        command_result = my_socket.receive_data()
+        print("[+] Command result is ----------------------------------------------------- \n", command_result)
 
